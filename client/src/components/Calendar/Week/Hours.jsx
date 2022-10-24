6167012ad25a2ed55a4ca14309946431e71f496e
Elton <rs.eltonribeiro@gmail.com>
2022-10-24 12:27:04 -0300

    Added hours into the week show

--- client/src/components/Calendar/Week/Hours.jsx
+++ client/src/components/Calendar/Week/Hours.jsx
@@ -1,7 +1,27 @@
+import { eachMinuteOfInterval } from "date-fns";
 import Styles from "./Hours.module.css";
 
 const Hours = () => {
-    
+  const stdDayMinutes = eachMinuteOfInterval(
+    {
+      start: new Date(2022, 1, 1, 6),
+      end: new Date(2022, 1, 1, 21)
+    }
+  )      
+
+  return (
+    <div className={Styles["hours"]}>
+      {
+        stdDayMinutes.map((hour) => {
+          if(hour.getMinutes() == 30 || hour.getMinutes() == 0){
+            return <div className={Styles["hours__block"]}>
+              {hour.toLocaleTimeString("pt-BR").slice(0,5)}
+            </div>
+          }
+        })
+      }
+    </div>
+  ) 
 }
 
 export default Hours;
--- client/src/components/Calendar/Week/Week.jsx
+++ client/src/components/Calendar/Week/Week.jsx
@@ -1,16 +1,20 @@
 import React, { useState } from "react";
 import CalendarNav from "../CalendarNav";
 import WeekHeader from "./WeekHeader";
+import Hours from "./Hours";
 import Styles from "./Week.module.css";
 
 const Week = (props) => { 
    return (
         <React.Fragment> 
-            <WeekHeader days={props.weekDays} />
+            <WeekHeader days={props.weekDays} /> 
             <div className={Styles["week"]}>
+              <div className={Styles["week__days"]}>
+                <Hours />
                 {props.weekDays.map((day) => {
-                    return <div>Dia</div>
+                    return <div className={Styles["week__day"]}>dia</div>
                 })}
+              </div> 
             </div>
         </React.Fragment>
    ) 
